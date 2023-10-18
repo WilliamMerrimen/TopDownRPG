52614d133bc426902a38ea14b8fccf388de9c3c8
Khon <merrimen03@gmail.com>
2023-10-19 00:14:02 +0700

    animation_0.1.0

--- TopDownRPG/Assets/Scripts/PlayerController.cs
+++ TopDownRPG/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,6 +13,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rb;
     private Animator _anim;
     private SpriteRenderer _spriteRenderer;
+
+    private bool _canMove = true;
+    private short _status = -1;
     
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     
@@ -24,28 +28,39 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (_movementInput != Vector2.zero)
+        if (_canMove)
         {
-            bool success = TryMove(_movementInput);
+            if (_movementInput != Vector2.zero)
+            {
+                bool success = TryMove(_movementInput);
 
-            if (!success)
-            {
-                success = TryMove(new Vector2(_movementInput.x, 0));
+                if (!success)
+                {
+                    success = TryMove(new Vector2(_movementInput.x, 0));
+                }
+
+                if (!success)
+                {
+                    TryMove(new Vector2(0, _movementInput.y));
+                }
+
+                _anim.SetBool("isMovingDown", success && (_movementInput.x == 0 && _movementInput.y < 0));
+                _anim.SetBool("isMovingUp", success && (_movementInput.x == 0 && _movementInput.y > 0));
+                _anim.SetBool("isMovingLeft", success && _movementInput.x != 0);
+                _spriteRenderer.flipX = _movementInput.x < 0;
+                if (_anim.GetBool("isMovingDown"))
+                    _status = -1;
+                if (_anim.GetBool("isMovingUp"))
+                    _status = 1;
+                if (_anim.GetBool("isMovingLeft"))
+                    _status = 0;
             }
-            if (!success)
+            else
             {
-                TryMove(new Vector2(0, _movementInput.y));
+                _anim.SetBool("isMovingLeft", false);
+                _anim.SetBool("isMovingDown", false);
+                _anim.SetBool("isMovingUp", false);
             }
-            _anim.SetBool("isMovingDown", success && (_movementInput.x == 0 && _movementInput.y < 0));
-            _anim.SetBool("isMovingUp", success &&(_movementInput.x == 0 && _movementInput.y > 0));
-            _anim.SetBool("isMovingLeft", success && _movementInput.x != 0);
-            _spriteRenderer.flipX = _movementInput.x < 0;
-        }
-        else
-        {
-            _anim.SetBool("isMovingLeft",false);
-            _anim.SetBool("isMovingDown", false);
-            _anim.SetBool("isMovingUp", false);
         }
     }
 
@@ -78,5 +93,25 @@ public class PlayerController : MonoBehaviour
     private void OnMove(InputValue movementValue)
     {
         _movementInput = movementValue.Get<Vector2>();
+    }
+
+    private void OnFire()
+    {
+        if(_status == -1)
+            _anim.SetTrigger("swordAtack");
+        if(_status == 1)
+            _anim.SetTrigger("swordAtackUP");
+        if(_status == 0)
+            _anim.SetTrigger("swordAtackLeft");
+    }
+
+    public void lockMovement()
+    {
+        _canMove = false;
+    }
+
+    public void unLockMovement()
+    {
+        _canMove = true;
     }
 }
