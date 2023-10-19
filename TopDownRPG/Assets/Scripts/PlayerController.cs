320ac41efcfdc150e29d7110f7bec5f7d174dfd0
Khon <merrimen03@gmail.com>
2023-10-19 18:05:00 +0700

    animation_0.1.1

--- TopDownRPG/Assets/Scripts/PlayerController.cs
+++ TopDownRPG/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 
 public class PlayerController : MonoBehaviour
@@ -16,14 +16,33 @@ public class PlayerController : MonoBehaviour
 
     private bool _canMove = true;
     private short _status = -1;
+
+    [FormerlySerializedAs("SwordAtackDown")] public GameObject swordAtackDown;
+    [FormerlySerializedAs("SwordAtackUp")] public GameObject swordAtackUp;
+    [FormerlySerializedAs("SwordAtackLeft")] public GameObject swordAtackLeft;
+
+    private float _swordAtackLeftPositionX;
+    private Collider2D _clSwordDown;
+    private Collider2D _clSwordUp;
+    private Collider2D _clSwordLeft;
     
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-    
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        _clSwordDown = swordAtackDown.GetComponent<Collider2D>();
+        _clSwordUp = swordAtackUp.GetComponent<Collider2D>();
+        _clSwordLeft = swordAtackLeft.GetComponent<Collider2D>();
+
+        _swordAtackLeftPositionX = swordAtackLeft.transform.position.x;
+
+        _clSwordDown.enabled = false;
+        _clSwordUp.enabled = false;
+        _clSwordLeft.enabled = false;
     }
 
     private void FixedUpdate()
@@ -35,19 +54,17 @@ public class PlayerController : MonoBehaviour
                 bool success = TryMove(_movementInput);
 
                 if (!success)
-                {
                     success = TryMove(new Vector2(_movementInput.x, 0));
-                }
 
                 if (!success)
-                {
                     TryMove(new Vector2(0, _movementInput.y));
-                }
 
                 _anim.SetBool("isMovingDown", success && (_movementInput.x == 0 && _movementInput.y < 0));
                 _anim.SetBool("isMovingUp", success && (_movementInput.x == 0 && _movementInput.y > 0));
                 _anim.SetBool("isMovingLeft", success && _movementInput.x != 0);
+                
                 _spriteRenderer.flipX = _movementInput.x < 0;
+                
                 if (_anim.GetBool("isMovingDown"))
                     _status = -1;
                 if (_anim.GetBool("isMovingUp"))
@@ -76,18 +93,14 @@ public class PlayerController : MonoBehaviour
                 moveSpeed * Time.fixedDeltaTime + collisionOfset);
             if (count == 0)
             {
-                _rb.MovePosition(_rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                _rb.MovePosition(_rb.position + direction * (moveSpeed * Time.fixedDeltaTime));
                 return true;
             }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
+
             return false;
         }
+
+        return false;
     }
 
     private void OnMove(InputValue movementValue)
@@ -105,13 +118,52 @@ public class PlayerController : MonoBehaviour
             _anim.SetTrigger("swordAtackLeft");
     }
 
-    public void lockMovement()
+    public void LockMovement()
     {
         _canMove = false;
     }
 
-    public void unLockMovement()
+    public void UnLockMovement()
     {
         _canMove = true;
+    }
+
+    public void Atack()
+    {
+        if (_status == -1)
+        {
+            _clSwordDown.enabled = true;
+            Debug.Log("Atack Down");
+        }
+
+        if (_status == 1)
+        {
+            _clSwordUp.enabled = true;
+            Debug.Log("Atack UP");
+        }
+
+        if(_status == 0)
+        {
+            if (_spriteRenderer.flipX)
+            {
+                swordAtackLeft.transform.position = new Vector2(_swordAtackLeftPositionX * -1, swordAtackLeft.transform.position.y);
+                Debug.Log("Atack Right");
+            }
+            else
+            {
+                swordAtackLeft.transform.position = new Vector2(_swordAtackLeftPositionX, swordAtackLeft.transform.position.y);
+                Debug.Log("Atack Left");
+            }
+
+            _clSwordLeft.enabled = true;
+        }
+                
+    }
+
+    public void StopAtack()
+    {
+        _clSwordDown.enabled = false;
+        _clSwordUp.enabled = false;
+        _clSwordLeft.enabled = false;
     }
 }
